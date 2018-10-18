9a70648a453ddfd226c4900e4613a0c611e8955c
Simon P. Ditner <spditner@gmail.com>
2018-10-18 05:55:51 -0400

    Note about similar programs

--- render.sh
+++ render.sh
@@ -10,3 +10,8 @@ render_template templates/test.conf b='123' c='dd, dd'
 echo "---------"
 render_template templates/include.conf b='foo' c='"dd, dd"'
 echo "---------"
+
+# NOTE(spditner/2018-11-18):
+#   There is a core command called 'envsubst' that can achieve something
+#   somewhat similar, where a cat'd files variables are replaced
+
